248a17102ada5cea42b40c64448ef5308a6675ac
cdwijayarathna <cdwijayarathna@gmail.com>
2015-04-25 02:18:09 +0530

    Added all methods after informal testing oj module

--- ext/org/jruby/ext/Oj.java
+++ ext/org/jruby/ext/Oj.java
@@ -19,10 +19,15 @@ public class Oj {
     
     @JRubyMethod(module = true)
     public static IRubyObject compat_load(IRubyObject self, IRubyObject json, IRubyObject options) {
-	System.out.println("Compat Load");
+	System.out.println("Compat Load with options");
         return null;
     }
-    //overload??
+    
+    @JRubyMethod(module = true)
+    public static IRubyObject compat_load(IRubyObject self, IRubyObject json) {
+	System.out.println("Compat Load without options");
+        return null;
+    }
 
 
     @JRubyMethod(module = true)
@@ -31,7 +36,7 @@ public class Oj {
         return null;
     }
 
-    @JRubyMethod(module = true)
+    @JRubyMethod(name = "default_options=",module = true)
     public static IRubyObject default_options(IRubyObject self, IRubyObject opts) {
 	System.out.println("Default Options 2");
         return null;
@@ -39,38 +44,57 @@ public class Oj {
 
     @JRubyMethod(module = true)
     public static IRubyObject dump(IRubyObject self, IRubyObject obj, IRubyObject options) {
-	System.out.println("Dump");
+	System.out.println("Dump with options");
         return null;
     }
-    //overload??
+   
+    @JRubyMethod(module = true)
+    public static IRubyObject dump(IRubyObject self, IRubyObject obj) {
+	System.out.println("Dump without options");
+        return null;
+    }
 
     @JRubyMethod(module = true)
     public static IRubyObject load(IRubyObject self, IRubyObject json, IRubyObject options) {
-	System.out.println("Load");
+	System.out.println("Load with options");
         return null;
     }
-    //overload??
+    
+    @JRubyMethod(module = true)
+    public static IRubyObject load(IRubyObject self, IRubyObject json) {
+	System.out.println("Load without options");
+        return null;
+    }
 
     @JRubyMethod(module = true)
     public static IRubyObject load_file(IRubyObject self, IRubyObject path, IRubyObject options) {
-	System.out.println("Load File");
+	System.out.println("Load File with options");
         return null;
     }
-    //overload??
+    
+    @JRubyMethod(module = true)
+    public static IRubyObject load_file(IRubyObject self, IRubyObject path) {
+	System.out.println("Load File without options");
+        return null;
+    }
 
     @JRubyMethod(module = true)
     public static IRubyObject mimic_JSON(IRubyObject self) {
 	System.out.println("mimic_JSON");
         return null;
     }
-    //overload??
 
     @JRubyMethod(module = true)
     public static IRubyObject object_load(IRubyObject self, IRubyObject json, IRubyObject options) {
-	System.out.println("Object Load");
+	System.out.println("Object Load with options");
         return null;
     }
-    //overload??
+    
+    @JRubyMethod(module = true)
+    public static IRubyObject object_load(IRubyObject self, IRubyObject json) {
+	System.out.println("Object Load without options");
+        return null;
+    }
 
     ////REGISTER ODD ARGUEMENTS UNCLEAR , can't have more than 3 arguements ##############
 
@@ -86,6 +110,7 @@ public class Oj {
         return null;
     }
 
+    //Parameters not very clear in documentation
     @JRubyMethod(module = true)
     public static IRubyObject saj_parse(IRubyObject self) {
 	System.out.println("Saj Parse");
@@ -100,24 +125,40 @@ public class Oj {
 
     @JRubyMethod(module = true)
     public static IRubyObject strict_load(IRubyObject self, IRubyObject json, IRubyObject options) {
-	System.out.println("Strict Load");
+	System.out.println("Strict Load with options");
         return null;
     }
-    //overload??
+    
+    @JRubyMethod(module = true)
+    public static IRubyObject strict_load(IRubyObject self, IRubyObject json) {
+	System.out.println("Strict Load without options");
+        return null;
+    }
 
    @JRubyMethod(module = true)
     public static IRubyObject to_file(IRubyObject self, IRubyObject file_path, IRubyObject obj, IRubyObject options) {
-	System.out.println("To File");
+	System.out.println("To File with options");
         return null;
     }
-    //overload??
+    
+
+    @JRubyMethod(module = true)
+    public static IRubyObject to_file(IRubyObject self, IRubyObject file_path, IRubyObject obj) {
+	System.out.println("To File without options");
+        return null;
+    }
 
     @JRubyMethod(module = true)
     public static IRubyObject to_stream(IRubyObject self, IRubyObject io, IRubyObject obj, IRubyObject options) {
-	System.out.println("To stream");
+	System.out.println("To stream with options");
         return null;
     }
-    //overload??
+    
+    @JRubyMethod(module = true)
+    public static IRubyObject to_stream(IRubyObject self, IRubyObject io, IRubyObject obj) {
+	System.out.println("To stream without options");
+        return null;
+    }
 
 
 }
