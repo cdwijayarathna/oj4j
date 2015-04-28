4e748869cede0b736d46e3f2924f571663f78a41
cdwijayarathna <cdwijayarathna@gmail.com>
2015-04-29 01:06:34 +0530

    Doc class unit tests

--- ext/test/org/jruby/ext/TestOjLibrary.java
+++ ext/test/org/jruby/ext/TestOjLibrary.java
@@ -1,26 +1,80 @@
 package org.jruby.ext;
 
+import org.jruby.Ruby;
+import org.jruby.RubyClass;
+
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestOjLibrary{
 
-    OjLibrary oj;
-    public TestOjLibrary(){
-        oj = new OjLibrary();
-    }
-
-    @Test
-    public void test_compat_load() {
-        String str= "Junit is working fine";
-        assertEquals("Junit is working fine",str);
+	@Test
+    public void test_doc_open() {
         
     }
 
-    @Test
-    public void test_compat_load1() {
-        int a = 1;
-        assertEquals(1,a);
+	@Test
+    public void test_doc_open_file() {
+        
+    }
+	
+	@Test
+    public void test_doc_close() {
+        
+    }
+	
+	@Test
+    public void test_doc_dump() {
+        
+    }
+	
+	@Test
+    public void test_doc_each_child() {
+        
+    }
+	
+	@Test
+    public void test_doc_each_value() {
+        
+    }
+	
+	@Test
+    public void test_doc_each_leaf() {
+        
+    }
+	
+	@Test
+    public void test_doc_fetch() {
+        
+    }
+	
+	@Test
+    public void test_doc_home() {
+        
+    }
+	
+	@Test
+    public void test_doc_local_key() {
+        
+    }
+	
+	@Test
+    public void test_doc_move() {
+        
+    }
+	
+	@Test
+    public void test_doc_size() {
+        
+    }
+	
+	@Test
+    public void test_doc_type() {
+        
+    }
+	
+	@Test
+    public void test_doc_where() {
         
     }
 
