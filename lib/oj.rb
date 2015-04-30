ce67e827744fc0764a15c113344df4f076d51e29
cdwijayarathna <cdwijayarathna@gmail.com>
2015-04-30 15:17:42 +0530

    removed BasicLibraryService

--- lib/oj.rb
+++ lib/oj.rb
@@ -7,6 +7,7 @@ require 'oj/schandler'
 module Oj
     if RUBY_PLATFORM =~ /java/
       require File.expand_path('../../bin/oj.jar', __FILE__)
+      Java::org.jruby.ext.OjLibrary.new.load(JRuby.runtime, false)
 
       def self.mask(*args)
         @mask ||= Sequence.new
