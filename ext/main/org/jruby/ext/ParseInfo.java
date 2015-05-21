ed467b532a218a1a4ffec25422ab608fa1d4d060
cdwijayarathna <cdwijayarathna@gmail.com>
2015-05-22 00:26:50 +0530

    Parsing objects not complete

---
+++ ext/main/org/jruby/ext/ParseInfo.java
@@ -0,0 +1,11 @@
+package org.jruby.ext;
+
+public class ParseInfo{
+	
+	private String json;
+	private String cur;
+	private String end;
+	
+	
+	
+}
---
+++ ext/main/org/jruby/ext/Reader.java
@@ -0,0 +1,18 @@
+package org.jruby.ext;
+
+public class Reader{
+	char base;
+	String head;
+	String tail;
+	String readEnd
+	String pro;
+	String str;
+	int line;
+	int col;
+	int freeHand;
+	
+	int readFunc(Reader reader){
+		
+	}
+	
+}
